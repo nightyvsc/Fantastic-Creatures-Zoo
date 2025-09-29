c4a41fc2966921f8db559625009a4fca016cb500
sebasbasto <sebas2006rod@gmail.com>
2025-09-29 16:55:36 -0500

    chore: importa base Spring Boot + MySQL (UTF-8) desde entorno local

---
+++ src/test/java/com/example/zoo_fantastico/ZooFantasticoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zoo_fantastico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooFantasticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
