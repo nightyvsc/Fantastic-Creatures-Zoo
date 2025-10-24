fc4961be7104f2064c40a7037e1145145997e2c9
santiago <belsanti0906@gmail.com>
2025-10-23 20:43:15 -0500

    integration test fix

--- src/main/java/com/example/zoo_fantastico/model/Creature.java
+++ src/main/java/com/example/zoo_fantastico/model/Creature.java
@@ -2,32 +2,36 @@ package com.example.zoo_fantastico.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
-import lombok.NoArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @NoArgsConstructor
 public class Creature {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank @Column(nullable = false)
+    @NotBlank
+    @Column(nullable = false)
     private String name;
 
-    @NotBlank @Column(nullable = false)
+    @NotBlank
+    @Column(nullable = false)
     private String species;
 
     @PositiveOrZero
     private double size;
 
-    @Min(1) @Max(10)
+    @Min(1)
+    @Max(10)
     private int dangerLevel;
 
     @NotBlank
     private String healthStatus;
 
-    @NotNull
-    @JoinColumn(name = "zone_id")
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "zone_id", nullable = false)
     private Zone zone;
 }
